110ee3011f95784e6982410371ec698ddbdbdc3a
saurabh kumar pandey <105418511+saurabhp0007@users.noreply.github.com>
2023-09-05 19:09:37 +0530

    Add files via upload

---
+++ WeatherApp 2.0  IOS/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by saurabh on 05/09/2023
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
